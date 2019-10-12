f1353b3e02d8762dbb4fba4f879a0c4b9477645b
matthpatth <thealexzaytsev@gmail.com>
2019-10-12 16:12:53 +0700

    commit changes

--- case1.py
+++ case1.py
@@ -1,6 +1,6 @@
 #project1
-capacity = (35000000) # water (world) km3
-capacityusa = (capacity*0.21) # water (usa) km3
-square = (9834) #km2
+capacity = (35000000*1000000000) # water (world) m3
+capacityusa = (capacity*0.21) # water (usa) m3
+square = (7664000*1000000) #m2
 height = capacityusa/square
 print(height)
